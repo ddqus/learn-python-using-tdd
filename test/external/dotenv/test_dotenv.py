f522b628ef8693fe3021704d2d1a80b096b0fc5d
ddqus <ddqus@users.noreply.github.com>
2022-03-07 10:15:28 +0900

    refact: .env 로딩은 conftest.py 에서 관리한다

--- test/external/dotenv/test_dotenv.py
+++ test/external/dotenv/test_dotenv.py
@@ -1,14 +1,5 @@
 import os
 
-from dotenv import load_dotenv
-
 
 def test_load():
-    # given
-    assert not os.getenv("DOTENV_DUMMY")
-
-    # when
-    load_dotenv()
-
-    # then
     assert "dotenv_dummy" == os.getenv("DOTENV_DUMMY")
